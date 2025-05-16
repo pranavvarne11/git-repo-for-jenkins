37be6bb51b93c3caad6686c526560bbb9028b3c3
pranavvarne11 <pranavvarne11@gmail.com>
2025-05-16 11:44:29 +0530

    cmt 1

--- test.sh
+++
@@ -1,6 +0,0 @@
-id 
-echo "This is Jenkins FreeStyle Job" 
-echo "This is Jenkins FreeStyle Job"
-printenv 
-ls -ltr 
-echo "This is the User Input Value for EnvironmentName parameter : $EnvironmentName"
